ff233b5df8ea52ba163038ee2cd0bf04632b765d
MatejGroombridge <matejdpg@gmail.com>
2021-09-22 09:01:02 +1000

    initial commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<h1>About</h1>
+<p>This is a really cool SvelteKit demo that uses the Pokemon API to build a pokedex</p>
+
+<svelte:head>
+	<title>About the SvelteKit Pokedex</title>
+</svelte:head>
---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { pokemon } from '../stores/pokestore';
+</script>
+
+<svelte:head>
+	<title>SvelteKit Pokedex</title>
+</svelte:head>
+
+<h1>SvelteKit Pokedex</h1>
+
+{#each $pokemon as pokeman}
+	<p>{pokeman.name}</p>
+{/each}
