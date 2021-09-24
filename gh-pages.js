b9df60224e31459e3470f32569c19af1f8a67b91
MatejGroombridge <matejdpg@gmail.com>
2021-09-24 10:38:22 +1000

    changed build directory

--- gh-pages.js
+++ gh-pages.js
@@ -1,7 +1,7 @@
 import { publish } from 'gh-pages';
 
 publish(
-	'public', // path to public directory
+	'/build', // path to public directory
 	{
 		branch: 'gh-pages',
 		repo: 'https://github.com/MatejGroombridge/SvelteKit-Pokedex-App.git', // Update to point to your repository
