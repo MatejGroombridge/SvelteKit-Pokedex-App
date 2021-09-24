85e60c1913b2b79e7768462079ac64fa25fa9d19
MatejGroombridge <matejdpg@gmail.com>
2021-09-24 10:21:14 +1000

    gh pages commit

---
+++ gh-pages.js
@@ -0,0 +1,17 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+	'public', // path to public directory
+	{
+		branch: 'gh-pages',
+		repo: 'https://github.com/MatejGroombridge/SvelteKit-Pokedex-App.git', // Update to point to your repository
+		user: {
+			name: 'Matej', // update to use your name
+			email: 'matejdpg@gmail.com' // Update to use your email
+		},
+		dotfiles: true
+	},
+	() => {
+		console.log('Deploy Complete!');
+	}
+);
